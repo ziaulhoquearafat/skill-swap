f6a8b8bdd1d790e163b5ca54b632ec76b3db8fde
Arafat <mdarafat3167@gmail.com>
2025-10-22 22:26:55 +0600

    setup context api

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
+import AuthProvider from "./context/AuthProvider";
 import "./index.css";
 import router from "./routes/Routes";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>
 );
