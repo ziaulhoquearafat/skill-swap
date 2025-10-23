dc9cc046a8a60c32de515cfb86447d5f4f82aecf
Arafat <mdarafat3167@gmail.com>
2025-10-24 03:29:09 +0600

    faq section implement

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,20 @@
+import Faq from "../components/FAQ/Faq";
+import HeroSlider from "../components/HeroSlider/HeroSlider";
+import HowItWorks from "../components/HowItWorks/HowItWorks";
+import MyContainer from "../components/MyContainer";
+import PopularSkills from "../components/PopularSkills/PopularSkills";
+import TopRatedProviders from "../components/TopRatedProviders/TopRatedProviders";
+
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <MyContainer>
+        <HeroSlider />
+        <PopularSkills />
+        <HowItWorks />
+        <TopRatedProviders />
+        <Faq />
+      </MyContainer>
     </div>
   );
 };
